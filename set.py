a0312362ac733bbb5388bdd994e64084ae456c95
indrekln <laan.indrek@gmail.com>
2015-11-23 04:55:55 +0200

    Alati set sees
    -valib pakist kaardid asemele, nii et lauale jääb vähemalt üks set
    (vahel ei leia ja jääb loopima)
    -panin programmi ajutiselt iseseisvalt kaarte valima
    -punktiarvestust korrigeeritud

--- set.py
+++ set.py
@@ -49,21 +49,9 @@ def find_sets(cards):
 
 def do_draw(amount=12): # märkides amountiks 81, tagastab funktsioon find_sets(ontable) kõik 1080 võimalikku setti.
     for i in range(amount):
-            on_table.append(gamedeck.pop())
+        on_table.append(gamedeck.pop())
 
-# Tegin järgnevate jaoks eraldi funktsioonid
-"""
-	tmp = find_sets(on_table)
-	print("Kaardid")
-	for i in on_table:
-		print(i)
-	print("Setid")
-	for i in tmp:
-		print(i)
-"""
-
-def remove_set(selection):
-    # ei kontrolli, kas valitakse kaart, mida pole laual - graafiline liides peaks välistama võimaluse
+def convert_selection(selection):
     # praegusel kujul tuleb sisend sõnena
     # laual olevate kaartide järjekorranumbrid
     sel = selection.replace(' ','').split(',')
@@ -73,10 +61,65 @@ def remove_set(selection):
 
     # sorteerib kaardid, et kontrollida, kas need on set
     sorted_cards = tuple(sorted(cards))
-    if sorted_cards in find_sets(cards):
-        for card in range(3):
-            # asendab kaardid pakist võetud uutega, nii et laual järjekord ei muutu
-            on_table[int(sel[card])-1] = gamedeck.pop()
+    if sorted_cards in find_sets(sorted_cards):
+        return sorted_cards
+
+# funktsioon otsib valitud kaartide asemele sobivad, et garanteerida laual vähemalt üks set
+def pick_set_cards(selected_cards):
+    required = []
+    temp_table = []
+
+    # loob koopia laual olevatest kaartidest
+    for a in on_table:
+        temp_table.append(a)
+    # võtab koopiast välja eemaldatavad kaardid
+    for b in selected_cards:
+        temp_table.remove(b)
+
+    # võtab järelejäänud kaartidest 2 kaarti ja otsib neile gamedeckist kolmanda, et moodustada set
+    while len(find_sets(temp_table)) == 0:
+        for c in range(len(temp_table)):
+            i=temp_table[c]
+            for d in range(c+1,len(temp_table)):
+                j=temp_table[d]
+                needed = []
+                for atr in range(4):
+                    if(i[atr]==j[atr]):
+                        needed.append(i[atr])
+                    else:
+                        needed.append((-i[atr]-j[atr])%3)
+                needed = (needed[0], needed[1], needed[2], needed[3])
+                if(needed in gamedeck):
+                    required.append(gamedeck.pop(gamedeck.index(needed)))
+
+                    # kui üks kaart olemas, lisatakse gamedeckist suvalised juurde
+                    while len(temp_table) + len(required) < len(on_table):
+                        required.append(gamedeck.pop())
+                    return required
+                else:
+                    print("-------------------------------------------------------------------")
+                    continue
+    # kui järelejäänute hulgas on set olemas, lisatakse lihtsalt vajalik hulk kaarte
+    while len(temp_table) + len(required) < len(on_table):
+        required.append(gamedeck.pop())
+    return required
+
+
+#funktsioon vahetab valitud kaartide asemele uued
+def replace_set(selection):
+    # ei kontrolli, kas valitakse kaart, mida pole laual - graafiline liides peaks välistama võimaluse
+    sel = selection.replace(' ','').split(',')
+    for card in range(3):
+        # asendab kaardid pakist võetud uutega, nii et laual järjekord ei muutu
+        on_table[int(sel[card])-1] = replace.pop()
+
+
+# programm valib laualt ise kaardid
+def set_index(set_values):
+    temp = list(set_values)
+    ret = str(on_table.index(temp[0][0])+1) + "," + str(on_table.index(temp[0][1])+1) + "," + str(on_table.index(temp[0][2])+1)
+    return ret
+
 
 def print_table():
     row = 1
@@ -86,24 +129,26 @@ def print_table():
         row += 1
     print("Laual on", str(len(find_sets(on_table))), "setti:")
     print(str(find_sets(on_table)))
-    print("Pakis on veel", len(gamedeck), "kaarti.")
+    print("Pakis on veel", len(gamedeck), "kaarti. Punktid:", str(score))
 
 on_table = []
 score = 0
 
 # Mängu alguses tõmmatakse 12 kaarti
-do_draw()
-
-# Mäng ei oska praegu arvestada 12 kaardi piiranguga, st valib pakist viimased kaardid ja ei arvesta, et 12 kaardi seas PEAB olema set
+do_draw(12)
+    
+# Alati ei jää viimaste kaartide hulka seti.
 while len(gamedeck) > 0:
     print_table()
     time_start = time()
     sets = len(find_sets(on_table))
     if sets > 0:
-        selection = input("Vali kaardid (kujul: 1,10,6): ")
-        remove_set(selection)
+        selection = set_index(find_sets(on_table)) #input("Vali kaardid (kujul: 1,10,6): ")
+        selected_cards = convert_selection(selection)
+        replace = pick_set_cards(selected_cards)
+        replace_set(selection)
         time_stop = time()
-        score += round(time_stop - time_start)//sets # Mida rohkem sette laual, seda vähem punkte
+        score += round(120 - (time_stop - time_start)//sets) # Mida rohkem sette laual, seda vähem punkte; praegu läheb ka miinusesse
     else:
         # seti puudumisel tõmbab täiendavad 3 kaarti; praegu jääb sellisel kujul mängu lõpuni 3 kaarti rohkem, kuna valitud kaardid asendatakse
         # poleks vaja, kui programm tõmbaks kaardid alati nii, et leiduks ka set
